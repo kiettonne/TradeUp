0bc57b6a9a34bfff3beb4b62335d7ce066328232
kiettonne <tuankietnguyen245@gmail.com>
2025-06-21 13:22:27 +0700

    connect FirebaseAuth

--- app/src/main/java/com/example/tradeup/MainActivity.java
+++ app/src/main/java/com/example/tradeup/MainActivity.java
@@ -1,24 +1,46 @@
 package com.example.tradeup;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
 
-import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class MainActivity extends AppCompatActivity {
+
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+        mAuth =  FirebaseAuth.getInstance();
+
+        String email = "tuankietnguyen245@gmail.com";
+        String pass = "Kiet123@567";
+
+        mAuth.createUserWithEmailAndPassword(email, pass).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if (task.isSuccessful()) {
+                    Log.d("Main", "createUserEmail:success");
+                    FirebaseUser user = mAuth.getCurrentUser();
+
+                    Toast.makeText(MainActivity.this, "", Toast.LENGTH_SHORT).show();
+                } else {
+                    Log.w("Main", "createUserEmail:fail");
+                    Toast.makeText(MainActivity.this, task.getException().getMessage(), Toast.LENGTH_SHORT).show();
+                }
+            }
         });
     }
+
 }
---
+++ app/src/main/java/com/example/tradeup/ui/auth/LoginActivity.java
@@ -0,0 +1,8 @@
+package com.example.tradeup.ui.auth;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class LoginActivity extends AppCompatActivity {
+
+
+}
